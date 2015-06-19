d98d01f6a01c6e352eb6c5e763f6e5726d999b67
tsmuse <tony@notjustprettypictures.com>
2015-06-18 18:06:43 -0700

    Beat my head against it for an hour and then figured out how to get the admin view to show a prettified RSVP name composed of all the guest_names in guest_set. Learninggit status

--- rsvpApp/admin.py
+++ rsvpApp/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import RsvpResponse, Guest
+
+class GuestInLine(admin.TabularInline):
+	model = Guest
+	extra = 0
+	list_display = ('guest_name','guest_attending', 'guest_drink_pref')
+
+def pretty_rsvp_name(obj):
+	my_name = "RSVP for "
+	for guest in obj.guest_set.all():
+		my_name += guest.guest_name + ", "	
+	return my_name
+
+
+class RsvpResponseAdmin(admin.ModelAdmin):
+	inlines = [GuestInLine]
+	list_display = (pretty_rsvp_name,)
+	
+
+
+
+
+admin.site.register(RsvpResponse, RsvpResponseAdmin)
--- rsvpApp/models.py
+++ rsvpApp/models.py
@@ -7,7 +7,14 @@ from django.utils import timezone
 
 class RsvpResponse(models.Model):
 	response_date = models.DateTimeField('responded on')
-	
+
+	""" 
+	what I want to do here is derrive the name of the RSVP from all the child guest_name values
+	but I can't figure out if it's possible or not. Can prettify in views, so this is a placeholder.
+	"""
+	def __str__(self):
+		return "RSVP " + str(self.id)
+
 
 class Guest(models.Model):
 	rsvp_response = models.ForeignKey(RsvpResponse)
