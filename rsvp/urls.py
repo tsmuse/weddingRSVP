46bd705f6b5ed353ee9d365d5656636e868dbdd7
tsmuse <tony@notjustprettypictures.com>
2015-06-28 16:36:57 -0700

    added a url pattern to the project urls.py to render the rsvpApp:Index view as the root page in the site.Since this is the only app in this project, it shouldn't be an issue.

--- rsvp/urls.py
+++ rsvp/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from rsvpApp import views
 
 urlpatterns = [
+	url(r'^$', views.IndexView.as_view()),
 	url(r'^rsvpApp/', include('rsvpApp.urls', namespace="rsvpApp")),
     url(r'^admin/', include(admin.site.urls)),
 ]
